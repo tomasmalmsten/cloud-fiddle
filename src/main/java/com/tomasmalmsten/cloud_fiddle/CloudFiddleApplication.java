a028aa73d7007ce5a5b00e0eb54c9ca0f76dbd84
Tomas Malmsten <code@tomasmalmsten.com>
2025-11-29 09:56:18 +0100

    Initial, spring initiliser, app

---
+++ src/main/java/com/tomasmalmsten/cloud_fiddle/CloudFiddleApplication.java
@@ -0,0 +1,13 @@
+package com.tomasmalmsten.cloud_fiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFiddleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomasmalmsten/cloud_fiddle/CloudFiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasmalmsten.cloud_fiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFiddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
