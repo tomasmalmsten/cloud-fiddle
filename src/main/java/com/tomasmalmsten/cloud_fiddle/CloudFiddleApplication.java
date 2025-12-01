29c1da45af3374cb8382bf747c77730e4d66c029
Tomas Malmsten <code@tomasmalmsten.com>
2025-12-01 08:17:20 +0100

    Add a hello controller so we have something to show a caller

--- src/main/java/com/tomasmalmsten/cloud_fiddle/CloudFiddleApplication.java
+++ src/main/java/com/tomasmalmsten/cloud_fiddle/CloudFiddleApplication.java
@@ -2,6 +2,8 @@ package com.tomasmalmsten.cloud_fiddle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CloudFiddleApplication {
@@ -9,5 +11,14 @@ public class CloudFiddleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudFiddleApplication.class, args);
 	}
+
+    @RestController
+    class Hello {
+
+        @GetMapping("/")
+        public String hello() {
+            return "hello"; // corresponds to hello.html in templates
+        }
+    }
 
 }
